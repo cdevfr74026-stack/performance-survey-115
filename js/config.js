b2bb5a601177a855bac2ae8d339364dc11695823
cdevfr74026-stack <cdevfr74026@gmail.com>
2026-08-14 22:22:55 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,11 +6,11 @@
  * 完成 Google Apps Script 部署後（見 README.md 步驟），
  * 把取得的 Web App 網址貼到下面 GAS_WEB_APP_URL 即可。
  * ------------------------------------------------------------
- */GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbz1Gje9kfbHb5sLR1e_PbhnOXodQthOvQVMJZ1_DhpGidVBooCzHAyJ2_obKkhCIi0R/exec',
+ */
 const CONFIG = {
   // 貼上您部署 Google Apps Script 後取得的網址
   // 範例：'https://script.google.com/macros/s/AKfycb.../exec'
-  GAS_WEB_APP_URL: 'PASTE_YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE',
+  GAS_WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbz1Gje9kfbHb5sLR1e_PbhnOXodQthOvQVMJZ1_DhpGidVBooCzHAyJ2_obKkhCIi0R/exec',
 
   // 問卷標題（首頁顯示）
   SURVEY_TITLE: '🎯 115上半年績效考核回饋問卷',
